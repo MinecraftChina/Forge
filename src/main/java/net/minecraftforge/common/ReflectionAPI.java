097da08afa05fa0553f4a864e171cd22f5bf59dc
unknown <yfgn0711@game.ntes>
2017-07-18 11:41:01 +0800

    fix bugs

--- src/main/java/net/minecraftforge/common/ReflectionAPI.java
+++ src/main/java/net/minecraftforge/common/ReflectionAPI.java
@@ -8,13 +8,7 @@ import org.apache.logging.log4j.Level;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 
-/**
- * @author 
- * 通过反射调用依赖的library。
- * 避免gradle编译失败，用反射方式调用library。
- */
 public class ReflectionAPI {
-	//检查是否有权限使用mod。
 	public static boolean checkPermission(ClassLoader loader, File modeFile) {
         try {
             final Class<?> clazz = Class.forName("net.minecraft.launchwrapper.LaunchClassLoader", false, loader);
@@ -24,30 +18,15 @@ public class ReflectionAPI {
 		} catch (Exception e) {
 			errorLog(String.format("check permission failed.message:%s.", e.toString()));
 			return true;
-		} 
-        return false;
+		}
 	}
-	//检查是否资源的md5.
-	/*
-	public static boolean checkResourceMd5(ClassLoader loader, File file) {
-        try {
-            final Class<?> clazz = Class.forName("net.minecraft.launchwrapper.LaunchClassLoader", false, loader);
-            final Method callMethod = clazz.getMethod("checkResourceMd5", new Class[]{File.class});
-            boolean bRet = (Boolean)callMethod.invoke(null, (Object)(file));
-            return bRet;
-		} catch (Exception e) {
-			errorLog(String.format("check md5 failed.message:%s.", e.toString()));
-		} 
-        return false;
-	}
-	*/
-	//获取解密后的mod字节码。
 	public static byte[] decrytClass(ClassLoader loader, byte[] classBytes) throws 
 			SecurityException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		byte[] decrytBytes;
 		try{
 			final Class<?> clazz = Class.forName("net.minecraft.launchwrapper.AESHelper", false, loader);
 			final Method callMethod = clazz.getMethod("Decrypt", new Class[]{byte[].class});
-			byte[] decrytBytes = (byte[])callMethod.invoke(null, (Object)classBytes);
+			decrytBytes = (byte[])callMethod.invoke(null, (Object)classBytes);
 		} catch (NoSuchMethodException e) {
 			return classBytes;
 		} 
